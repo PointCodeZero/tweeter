d73b0fc2a87f937bcf1cbd88235975aa0816afdb
Lucas Cruz <lucas_ftc@hotmail.com>
2019-01-22 22:30:13 +0000

    add textarea counter functionality

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,16 @@
-$('document').ready(() => { console.log("Working") });
+$(document).ready(function() {
+
+  const maxLength = 140;
+
+  $('form textarea').on('keyup', function() {
+    let currentLength = $(this).val().length;
+    let formLength = maxLength - currentLength;
+    let counter = $(this).siblings('.counter').text(formLength);
+    if (formLength >= 0) {
+      counter.css('color', '#244751');
+    } else {
+      counter.css('color', 'red');
+    }
+  });
+
+});
