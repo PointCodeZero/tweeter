b5c949638cf9cd1d91de671babb5097c55816f5f
Lucas Cruz <lucas_ftc@hotmail.com>
2019-01-26 22:27:16 +0000

    add unlimeted like button feature with data persinstence to DB

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -29,7 +29,8 @@ module.exports = function(DataHelpers) {
       content: {
         text: req.body.text
       },
-      created_at: Date.now()
+      created_at: Date.now(),
+      likes: 0
     };
 
     DataHelpers.saveTweet(tweet, (err) => {
@@ -38,6 +39,17 @@ module.exports = function(DataHelpers) {
       } else {
         res.status(201).send();
       }
+    });
+  });
+
+  tweetsRoutes.post("/:id/like", function(req, res) {
+
+    DataHelpers.updateLikes(req.params.id, (err, tweet) => {
+      if (err) {
+        return res.status(400).json({ error: err.message });
+      }
+      console.log(tweet.value)
+      res.json(tweet.value);
     });
   });
 
